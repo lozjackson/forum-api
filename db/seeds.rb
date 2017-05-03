22c8f70c8ad71b49120fa8767eaa4513be05a935
Loz Jackson <lozjackson@talktalk.net>
2017-05-03 16:09:20 +0100

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,13 +10,13 @@
 # rake db:migrate:reset
 #
 
-author1 = User.create(username: "loz", name: "Loz Jackson")
-author2 = User.create(username: "robert", name: "Roberta Rock")
-author3 = User.create(username: "daniel", name: "Daniel Duck")
-author4 = User.create(username: "amanda", name: "Amanda Djidjinski")
-author5 = User.create(username: "zoe", name: "Zoe Zack")
-author6 = User.create(username: "bill", name: "Bill Burray")
-author7 = User.create(username: "charlie", name: "Charlie Chuck")
+author1 = User.create(username: "admin", name: "Administrator", password: 'adminpassword')
+author2 = User.create(username: "loz", name: "Loz Jackson", password: '1234')
+author3 = User.create(username: "daniel", name: "Daniel Duck", password: 'password')
+author4 = User.create(username: "amanda", name: "Amanda Djidjinski", password: 'password')
+author5 = User.create(username: "zoe", name: "Zoe Zack", password: 'password')
+author6 = User.create(username: "bill", name: "Bill Burray", password: 'password')
+author7 = User.create(username: "charlie", name: "Charlie Chuck", password: 'password')
 
 topic1 = Topic.create(title: "The Great Escape", user: author7, body: 'test')
 topic2 = Topic.create(title: "Saving Myself", user: author6, body: 'test')
