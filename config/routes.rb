b3fd9d4695702f64889f6245a086586e6a98b31a
Loz Jackson <lozjackson@talktalk.net>
2017-05-03 17:28:01 +0100

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get '/users/current-user', to: "current_user#show"
   resources :users
   resources :posts
   resources :topics
+  post '/login', to: "sessions#create"
+  post '/token-refresh', to: "sessions#refresh"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
