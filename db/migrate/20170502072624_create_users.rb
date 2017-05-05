35fae165378e107284c2823c536275a06d87f37e
Loz Jackson <lozjackson@talktalk.net>
2017-05-05 19:34:35 +0100

    update db

--- db/migrate/20170502072624_create_users.rb
+++ db/migrate/20170502072624_create_users.rb
@@ -3,8 +3,6 @@ class CreateUsers < ActiveRecord::Migration[5.1]
     create_table :users do |t|
       t.string :username
       t.string :name
-      t.string :password
-      t.string :salt
       t.boolean :disabled
       t.string :email
 
--- db/schema.rb
+++ db/schema.rb
@@ -34,8 +34,6 @@ ActiveRecord::Schema.define(version: 20170505090445) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "name"
-    t.string "password"
-    t.string "salt"
     t.boolean "disabled"
     t.string "email"
     t.datetime "created_at", null: false
